5f86c736131265649d9ab9f3183790dba291d185
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-08-06 22:48:44 +0300

    update tests

--- tests/src/GatewayTest.php
+++ tests/src/GatewayTest.php
@@ -3,14 +3,20 @@
 namespace ByTIC\Omnipay\Librapay\Tests;
 
 use ByTIC\Omnipay\Librapay\Gateway;
+use Guzzle\Http\Client;
 
+/**
+ * Class GatewayTest
+ * @package ByTIC\Omnipay\Librapay\Tests
+ */
 class GatewayTest extends AbstractTest
 {
     public function testPurchase()
     {
-        $gateway = new Gateway();
+        $httpClient = new Client();
+        $gateway = new Gateway($httpClient);
 
-        $parameters = require TEST_FIXTURE_PATH . 'simpleOrderParams.php';
+        $parameters = require TEST_FIXTURE_PATH . DIRECTORY_SEPARATOR. 'simpleOrderParams.php';
 
         foreach (['merchant', 'merchantName', 'merchantEmail', 'merchantUrl', 'terminal', 'key'] as $field) {
             $parameters[$field] = $_ENV['LIBRAPAY_' . strtoupper($field)];
@@ -19,8 +25,17 @@ class GatewayTest extends AbstractTest
         $request = $gateway->purchase($parameters);
         $response = $request->send();
 
-// Send the Symfony HttpRedirectResponse
-        $response->send();
+        $requestGateway = $httpClient->createRequest(
+            $response->getRedirectMethod(),
+            $response->getRedirectUrl(),
+            null,
+            $response->getRedirectData()
+        );
+        $responseGateway = $httpClient->send($requestGateway);
+        $htmlGateway = $responseGateway->getBody(true);
 
+        self::assertContains('Date comanda',$htmlGateway);
+        self::assertContains('Suma de plata:',$htmlGateway);
+        self::assertContains('3.49',$htmlGateway);
     }
 }
